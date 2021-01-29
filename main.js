b6a6d8b8af41cd59b6c5dc5d595becf7f1d7c446
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-01-29 21:25:15 +0530

    --- Auto-Git Commit ---

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log('Hi');
-console.error('This is an error');
-console.warn('warning!');
+//var, let, const
+//var is globally scoped and you dont wanna use it for the most parts
+//let and const have a block-level scope
+//with let u can reassign values
+
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const a = 30;
+a = 31;
+console.log(a);
