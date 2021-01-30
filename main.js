482bcf8f1ca92bb75a0e6d6c859b1c54bb3d508a
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-01-30 10:36:40 +0530

    var, let & const

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ let age = 30;
 age = 31;
 console.log(age);
 
-const a = 30;
+/*const a = 30;
 a = 31;
-console.log(a);
+console.log(a);*/
+
+//just use const if you're not sure you'll reassign it
+
+let score;
+score = 10;
+console.log(score);
